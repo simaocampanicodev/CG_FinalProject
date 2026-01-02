75fe3d2a6345d908502a76e50da883ad86c1aade
txger. <simaocampanico.dev@gmail.com>
2026-01-02 14:05:51 +0000

    Created a script for the PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  [SerializeField] private float moveSpeed = 5f;
+
+  private Rigidbody rb;
+  private Vector3 input;
+
+  private void Awake()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  private void Update()
+  {
+    float h = Input.GetAxis("Horizontal");
+    float v = Input.GetAxis("Vertical");
+
+    input = transform.right * h + transform.forward * v;
+  }
+
+  private void FixedUpdate()
+  {
+    Vector3 velocity = new Vector3(
+        input.x * moveSpeed,
+        rb.velocity.y,
+        input.z * moveSpeed
+    );
+
+    rb.velocity = velocity;
+  }
+}
